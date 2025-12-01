38b5ba2f227af14afb63383db0b17fef1f1e3d82
4za4s <arbo3802@gmail.com>
2025-12-01 19:51:04 +1100

    but what if it equals 0

--- 2025/1/safe cracker.py
+++ 2025/1/safe cracker.py
@@ -3,19 +3,28 @@ line = FileInput.readline()
 
 dial = 50
 TimesPointedAtZero = 0
+TimesPastZero = 0
 direction = 1
 
-def rotate(dial, distance):
-    dial += distance
-    dial = dial % 100
-    return dial
+def rotate(dial, distance, TimesPastZero):
+    dialNew = dial + distance
+
+    TimesPastZero += abs(distance) // 100
+    if dial != 0:
+        if distance < 0:
+            TimesPastZero += 1 if abs(distance) % 100 >= dial else 0
+        elif distance > 0:
+            TimesPastZero += 1 if distance % 100 >= 100 - dial else 0
+
+    dial = dialNew % 100
+    return dial, TimesPastZero
 
 while(line):
     line.strip()
 
     direction = -1 if line[0] == 'L' else 1
-    dial = rotate(dial, int(line[1:])*direction)
-    #print(dial)
+    dial, TimesPastZero = rotate(dial, int(line[1:])*direction, TimesPastZero)
+    #print("\n", dial, TimesPastZero)
 
     if dial == 0:
         TimesPointedAtZero += 1
@@ -23,4 +32,5 @@ while(line):
     line = FileInput.readline()
 
 print(TimesPointedAtZero)
- 
+print(TimesPastZero)
+
